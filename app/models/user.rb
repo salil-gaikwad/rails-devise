b89a55ca67e609fa36e183901feb3689075a0d27
salil-gaikwad <salilgaikwad@gmail.com>
2022-12-25 14:41:58 +0530

    Added code for rails devise authentication

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  self.skip_time_zone_conversion_for_attributes = [:created_at]
+end
